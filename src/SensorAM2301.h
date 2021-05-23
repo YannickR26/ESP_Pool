db31bb49611a1a9e82cdb2393397452379c8cd18
Yannick RICHARDOT <Yannick.richardot@carbonbee.fr>
2021-05-23 14:07:28 +0200

    Update Sensor...

--- src/SensorAM2301.h
+++ src/SensorAM2301.h
@@ -1,3 +1,11 @@
+/*
+ *  SensorAM2301.h
+ *
+ *  Created on: 09/05/2021
+ * 
+ *  Author : Yannick RICHARDOT (yannick.richardot@carbonbee.fr)
+ */
+
 #pragma once
 
 #include <DHTesp.h>
@@ -5,14 +13,28 @@
 class SensorAM2301
 {
 public:
-    SensorAM2301(uint8_t pin);
-    virtual ~SensorAM2301();
+    SensorAM2301(uint8_t pin)
+    {
+        _sensor.setup(pin, DHTesp::DHT22);
+    }
 
-    float readTemp();
-    float readHumidity();
-    const char *getStatus();
+    virtual ~SensorAM2301() {}
 
-protected:
+    float readTemp()
+    {
+        return _sensor.getTemperature();
+    }
+
+    float readHumidity()
+    {
+        return _sensor.getHumidity();
+    }
+
+    const char *getStatus()
+    {
+        return _sensor.getStatusString();
+    }
+
 private:
     DHTesp _sensor;
 };
