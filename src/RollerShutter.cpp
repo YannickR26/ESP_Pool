153d8d87620feda5c0cc2cbba783412c92b689ea
Yannick RICHARDOT <Yannick.richardot@carbonbee.fr>
2021-07-24 14:53:37 +0200

    refactor RollerShutter, add current position

--- src/RollerShutter.cpp
+++ src/RollerShutter.cpp
@@ -1,3 +1,5 @@
+#include <ArduinoJson.h>
+
 #include "RollerShutter.h"
 #include "Mqtt.h"
 #include "Logger.h"
@@ -7,65 +9,160 @@
 /********************************************************/
 RollerShutter::RollerShutter(uint8_t pinOpen, uint8_t pinClose)
 {
-    _pinOpen = pinOpen;
-    _pinClose = pinClose;
-    pinMode(_pinOpen, OUTPUT);
-    pinMode(_pinClose, OUTPUT);
-    digitalWrite(_pinOpen, 0);
-    digitalWrite(_pinClose, 0);
-    _timeout = 0;
+  _pinOpen = pinOpen;
+  _pinClose = pinClose;
+  pinMode(_pinOpen, OUTPUT);
+  pinMode(_pinClose, OUTPUT);
+  digitalWrite(_pinOpen, 0);
+  digitalWrite(_pinClose, 0);
+  _duration = 0.f;
+  _position = 0;  // 0 => Close, 100 => Open
+  _direction = 0; // -1 => to Close, 0 => Stop, 1 => to Open
+  _oldTick = 0;
 }
 
 RollerShutter::~RollerShutter()
 {
 }
 
-void RollerShutter::setTimeout(uint16_t timeInSeconds)
+void RollerShutter::handle()
 {
-    _timeout = timeInSeconds;
+  static unsigned long oldTickPublish;
+  const float deltaT = abs(millis() - _oldTick) / 1000;
+  const float movePosition = (deltaT / _duration) * 100;
+  _oldTick = millis();
+
+  // Open
+  if (_direction == 1)
+  {
+    _position += movePosition;
+    if (_position > 100.f)
+      _position = 100.f;
+    if (_position >= _target)
+      stop();
+  }
+  // Close
+  else if (_direction == -1)
+  {
+    _position -= movePosition;
+    if (_position < 0.f)
+      _position = 0.f;
+    if (_position <= _target)
+      stop();
+  }
+
+  // Publish state every 1 seconds
+  if ((millis() - oldTickPublish) >= (1 * 1000)) {
+    oldTickPublish = millis();
+    publishState();
+  }
+
+}
+
+void RollerShutter::setDuration(const float timeInSeconds)
+{
+  _duration = timeInSeconds;
+}
+
+void RollerShutter::setCurrentPosition(const float position)
+{
+  _position = position;
+}
+
+// 0 %   => Close
+// 100 % => Open
+void RollerShutter::setPosition(const float positionInPercent)
+{
+  if ((int)_position == (int)positionInPercent)
+    return;
+
+  _target = positionInPercent;
+
+  if (_position < _target)
+    _direction = 1;
+  else
+    _direction = -1;
+
+  // Compute the time to go to the target
+  const float timeToStop = abs(_position - _target) * _duration / 100.f;
+  _tickerStop.detach();
+  _tickerStop.once(timeToStop, +[](RollerShutter *inst) { inst->stop(true); }, this);
+
+  move();
 }
 
 void RollerShutter::open()
 {
-    digitalWrite(_pinClose, 0);
-    delay(100);
-    digitalWrite(_pinOpen, 1);
-
-    Log.println("Open rollerShutter !");
-    MqttClient.publish(String("rollerShutter"), String("open"));
-
-    _tickTimeout.detach();
-
-    if (_timeout > 0)
-        _tickTimeout.once(_timeout, +[](RollerShutter *inst) { inst->stop(); }, this);
-}
-
-void RollerShutter::stop()
-{
-    digitalWrite(_pinClose, 0);
-    digitalWrite(_pinOpen, 0);
-
-    Log.println("Stop rollerShutter !");
-    MqttClient.publish(String("rollerShutter"), String("stop"));
-
-    _tickTimeout.detach();
+  setPosition(100);
 }
 
 void RollerShutter::close()
 {
-    digitalWrite(_pinOpen, 0);
-    delay(100);
-    digitalWrite(_pinClose, 1);
+  setPosition(0);
+}
 
-    Log.println("Close rollerShutter !");
-    MqttClient.publish(String("rollerShutter"), String("close"));
+void RollerShutter::stop(bool forced)
+{
+  _direction = 0;
 
-    _tickTimeout.detach();
+  if (forced)
+    _position = _target;
 
-    if (_timeout > 0)
-        _tickTimeout.once(_timeout, +[](RollerShutter *inst) { inst->stop(); }, this);
+  move();
 }
 
 /********************************************************/
 /******************** Private Method ********************/
 /********************************************************/
+
+void RollerShutter::move()
+{
+  if (_direction == 1)
+  {
+    digitalWrite(_pinClose, 0);
+    delay(100);
+    digitalWrite(_pinOpen, 1);
+    _oldTick = millis();
+    Log.println("Open rollerShutter...");
+  }
+  else if (_direction == -1)
+  {
+    digitalWrite(_pinOpen, 0);
+    delay(100);
+    digitalWrite(_pinClose, 1);
+    _oldTick = millis();
+    Log.println("Close rollerShutter...");
+  }
+  else {
+    digitalWrite(_pinClose, 0);
+    digitalWrite(_pinOpen, 0);
+    Log.println("Stop rollerShutter !");
+  }
+
+  publishState(true);
+}
+
+void RollerShutter::publishState(bool forced)
+{
+  // Force publish state every 5 minutes
+  static unsigned long oldTick;
+  if ((millis() - oldTick) >= (5 * 60 * 1000)) {
+    oldTick = millis();
+    forced = true;
+  }
+
+  if (_direction == 0 && !forced)
+    return;
+
+  DynamicJsonDocument json(256);
+
+  json["position"] = (int)_position;
+  json["direction"] = _direction;
+  json["target"] = (int)_target;
+
+  String jsonStr;
+  serializeJson(json, jsonStr);
+  Log.println(jsonStr);
+
+  MqttClient.publish(String("rollerShutter"), jsonStr);
+}
--- src/RollerShutter.h
+++ src/RollerShutter.h
@@ -9,14 +9,25 @@ public:
     RollerShutter(uint8_t pinOpen, uint8_t pinClose);
     virtual ~RollerShutter();
 
-    void setTimeout(uint16_t timeInSeconds);
+    void handle();
+
+    void setDuration(const float timeInSeconds);
+    void setCurrentPosition(const float position);
+    void setPosition(const float positionInPercent);
     void open();
-    void stop();
     void close();
+    void stop(bool forced = false);
 
 protected:
 private:
+    void move();
+    void publishState(bool forced = false);
+
     uint8_t _pinOpen, _pinClose;
-    uint16_t _timeout;
-    Ticker _tickTimeout;
+    float _duration;    // in Seconds
+    float _position;    // in %
+    float _target;      // in %
+    int8_t _direction;
+    unsigned long _oldTick;
+    Ticker _tickerStop;
 };
